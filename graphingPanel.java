8836a5ebe24df33501a3ceab5e6a603d3453d18f
Eshan-K-I <79593316+Eshan-K-I@users.noreply.github.com>
2024-05-10 10:01:40 -0400

    get ready for graphing calc subpanel

--- graphingPanel.java
+++ graphingPanel.java
@@ -1,3 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import javax.swing.border.*;
+import java.util.ArrayList;
+
+// Use StackOverflow thing i found 
+
 public class graphingPanel extends JPanel {
 
 }
